757c6cec76b5b2729b673c93f6940bebf6663535
John Dean <john.holmes.dean@gmail.com>
2014-04-30 21:55:54 -0600

    Passing tests better

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by jdean on 4/28/14.
+ */
+
+
+exports.server = require('./lib/server');
+exports.target = require('./lib/target');
+exports.controller = require('./lib/controller');
+exports.socket = require('./lib/socket');
